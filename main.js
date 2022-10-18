b8357669a23355cc21f78c2c018a8c975330dcaf
AlexLimaValle <alexlimavalle@gmail.com>
2022-10-18 08:27:44 -0300

    3

--- main.js
+++ main.js
@@ -45,4 +45,41 @@ window.addEventListener("scroll",()=>{
 
 //stock:
 
+const $stockLaptop = document.querySelector(".contenedor__laptop");
 
+const $fragmento = document.createDocumentFragment();
+
+function recorrerArray(array){
+    array.forEach((e)=>{
+        const $laptop = document.createElement("DIV");
+        $laptop.innerHTML = `
+        <div class="contenedor__laptopMarca">
+            <img src="" alt="asus 245g8" class="contenedor__marcaImg">
+        </div>
+        <div class="contenedor__laptopPantalla">
+            <img src="" alt="pantalla" class="contenedor__pantallaImg">
+        </div>
+        <div class="contenedor__laptopImagen">
+            <img src="./img/laptop/hp/${e.modelo}.jpg" alt="imagen" class="contenedor__laptopImg">
+        </div>
+        <h4 class="contenedor__laptopTitulo">${e.nombre}</h4>
+        <div class="contenedor__laptopComponentes">
+            <p class="contenedor__laptopComponente">${e.disco}</p>
+            <p class="contenedor__laptopComponente">${e.pantalla}</p>
+            <p class="contenedor__laptopComponente">${e.ram}</p>
+            <p class="contenedor__laptopComponente">${e.procesador}</p>
+            <p class="contenedor__laptopComponente">${e.grafico}</p>
+            <p class="contenedor__laptopComponente">${e.sistemaOperativo}</p>
+        </div>
+        <div class="contenedor__precios">
+            <p class="contenedor__precio">${e.precio}</p>
+        </div>
+        <button class="contenedor__boton">Comprar</button>
+        `;
+        $fragmento.appendChild($laptop);        
+    })
+}
+
+recorrerArray(stock);
+
+$stockLaptop.appendChild($fragmento);
--- productos.js
+++ productos.js
@@ -16,6 +16,6 @@ class Laptop{
 
 }
 
-const hp245g8 = new Laptop("HP 245G8",14,"245G8","HP","256GB SSD","FULL HD","RYZEN 5 3500U","16GB","140000","INTEL IRIS","WINDOW 11");
+const hp245g8 = new Laptop("HP 245G8",14,"245g8","HP","256GB SSD","FULL HD","RYZEN 5 3500U","16GB","140000","INTEL IRIS","WINDOW 11");
 
 const stock = [hp245g8];
